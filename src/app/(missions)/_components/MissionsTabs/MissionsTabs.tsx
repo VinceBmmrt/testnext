b1d5602e4db7db287526fc0312e654e78096a791
VinceBmmrt <vincebommert75@gmail.com>
2024-02-14 22:46:14 +0100

    Custom color for the tabs

--- src/app/(missions)/_components/MissionsTabs/MissionsTabs.tsx
+++ src/app/(missions)/_components/MissionsTabs/MissionsTabs.tsx
@@ -1,79 +1,77 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const MissionsTabs = () => {
+  const [activeTab, setActiveTab] = useState("toProvideMissions");
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
   return (
     <div className="flex justify-center items-center h-full">
       <div className="w-full max-w-screen-md">
         <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
           <ul
             className="flex flex-wrap -mb-px text-sm font-medium text-center"
-            id="default-styled-tab"
-            data-tabs-toggle="#default-styled-tab-content"
-            data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500"
-            data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300"
             role="tablist"
           >
             <li className="me-2" role="presentation">
               <button
-                className="inline-block p-4 border-b-2 rounded-t-lg"
-                id="toProvideMissions-styled-tab"
-                data-tabs-target="#styled-toProvideMissions"
-                type="button"
-                role="tab"
-                aria-controls="toProvideMissions"
-                aria-selected="false"
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "toProvideMissions"
+                    ? "border-custom-color text-custom-color"
+                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500"
+                }`}
+                onClick={() => handleTabClick("toProvideMissions")}
               >
                 À pourvoir
               </button>
             </li>
             <li className="me-2" role="presentation">
               <button
-                className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="dashboard-styled-tab"
-                data-tabs-target="#styled-dashboard"
-                type="button"
-                role="tab"
-                aria-controls="dashboard"
-                aria-selected="false"
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "dashboard"
+                    ? "border-custom-color text-custom-color"
+                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500"
+                }`}
+                onClick={() => handleTabClick("dashboard")}
               >
                 En cours
               </button>
             </li>
             <li className="me-2" role="presentation">
               <button
-                className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="settings-styled-tab"
-                data-tabs-target="#styled-settings"
-                type="button"
-                role="tab"
-                aria-controls="settings"
-                aria-selected="false"
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "settings"
+                    ? "border-custom-color text-custom-color"
+                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500"
+                }`}
+                onClick={() => handleTabClick("settings")}
               >
                 À venir
               </button>
             </li>
-            <li role="presentation">
+            <li className="me-2" role="presentation">
               <button
-                className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="finishedMissions-styled-tab"
-                data-tabs-target="#styled-finishedMissions"
-                type="button"
-                role="tab"
-                aria-controls="finishedMissions"
-                aria-selected="false"
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "finishedMissions"
+                    ? "border-custom-color text-custom-color"
+                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500"
+                }`}
+                onClick={() => handleTabClick("finishedMissions")}
               >
                 Terminés
               </button>
             </li>
             <li role="presentation">
               <button
-                className="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="contacts-styled-tab"
-                data-tabs-target="#styled-contacts"
-                type="button"
-                role="tab"
-                aria-controls="contacts"
-                aria-selected="false"
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "contacts"
+                    ? "border-custom-color text-custom-color"
+                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 text-gray-500"
+                }`}
+                onClick={() => handleTabClick("contacts")}
               >
                 Annulé
               </button>
@@ -82,13 +80,12 @@ const MissionsTabs = () => {
         </div>
         <div id="default-styled-tab-content">
           <div
-            className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            id="styled-toProvideMissions"
-            role="tabpanel"
-            aria-labelledby="toProvideMissions-tab"
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === "toProvideMissions" ? "block" : "hidden"
+            }`}
           >
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
+              This is some placeholder content for the{" "}
               <strong className="font-medium text-gray-800 dark:text-white">
                 toProvideMissions tab's associated content
               </strong>
@@ -98,13 +95,12 @@ const MissionsTabs = () => {
             </p>
           </div>
           <div
-            className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            id="styled-dashboard"
-            role="tabpanel"
-            aria-labelledby="dashboard-tab"
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === "dashboard" ? "block" : "hidden"
+            }`}
           >
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
+              This is some placeholder content for the{" "}
               <strong className="font-medium text-gray-800 dark:text-white">
                 Dashboard tab's associated content
               </strong>
@@ -114,13 +110,12 @@ const MissionsTabs = () => {
             </p>
           </div>
           <div
-            className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            id="styled-settings"
-            role="tabpanel"
-            aria-labelledby="settings-tab"
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === "settings" ? "block" : "hidden"
+            }`}
           >
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
+              This is some placeholder content for the{" "}
               <strong className="font-medium text-gray-800 dark:text-white">
                 Settings tab's associated content
               </strong>
@@ -130,13 +125,12 @@ const MissionsTabs = () => {
             </p>
           </div>
           <div
-            className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            id="styled-finishedMissions"
-            role="tabpanel"
-            aria-labelledby="finishedMissions-tab"
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === "finishedMissions" ? "block" : "hidden"
+            }`}
           >
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
+              This is some placeholder content for the{" "}
               <strong className="font-medium text-gray-800 dark:text-white">
                 finishedMissions tab's associated content
               </strong>
@@ -146,15 +140,14 @@ const MissionsTabs = () => {
             </p>
           </div>
           <div
-            className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            id="styled-settings"
-            role="tabpanel"
-            aria-labelledby="settings-tab"
+            className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
+              activeTab === "contacts" ? "block" : "hidden"
+            }`}
           >
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
+              This is some placeholder content for the{" "}
               <strong className="font-medium text-gray-800 dark:text-white">
-                Settings tab's associated content
+                Contacts tab's associated content
               </strong>
               . Clicking another tab will toggle the visibility of this one for
               the next. The tab JavaScript swaps classes to control the content
@@ -163,6 +156,14 @@ const MissionsTabs = () => {
           </div>
         </div>
       </div>
+      <style jsx>{`
+        .border-custom-color {
+          border-color: #798c73; /* Change this to your desired color */
+        }
+        .text-custom-color {
+          color: #798c73; /* Change this to your desired color */
+        }
+      `}</style>
     </div>
   );
 };
