96c514a58f8d7bdc1c01aeb2423a08cdcc5c0e78
VinceBmmrt <vincebommert75@gmail.com>
2024-02-15 14:39:02 +0100

    bugfix css

--- src/app/(missions)/_components/MissionsSideBar/MissionsSideBar.tsx
+++ src/app/(missions)/_components/MissionsSideBar/MissionsSideBar.tsx
@@ -52,7 +52,7 @@ const MissionsSideBar = () => {
             <li>
               <a
                 href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group hidden sm:flex"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group hidden xl2:flex"
               >
                 <div
                   style={{ backgroundColor: "#798c73" }}
